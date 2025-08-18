9f45d2d0073ecac81d000299b9148c49a19cb29a
Didier Franc <didierfranc@icloud.com>
2025-08-18 16:18:52 +0200

    fix: try to block gpt agent

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -2,8 +2,17 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ url }) => {
 	return new Response(
-		`User-agent: *
-Disallow: 
+		`User-agent: GPTBot
+Disallow: /
+
+User-agent: ChatGPT-User
+Disallow: /
+
+User-agent: OAI-SearchBot
+Disallow: /
+
+User-agent: *
+Disallow:
 
 Sitemap: ${new URL('/sitemap.xml', url.origin).toString()}`,
 		{ status: 200, headers: { 'Content-Type': 'text/plain' } }
